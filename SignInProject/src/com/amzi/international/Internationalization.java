c89d65968bea0f42ae801aba379e63e67ada7cf3
pieh0001 <pieh0001@algonquinlive.com>
2015-10-26 00:27:00 -0400

    Put stuff in those files I added in my last Commit

--- SignInProject/src/com/amzi/international/Internationalization.java
+++ SignInProject/src/com/amzi/international/Internationalization.java
@@ -1,6 +1,34 @@
 package com.amzi.international;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Internationalization {
 
-	
+	public static void main(String[] args) throws Exception {
+
+		ResourceBundle bundle1 = ResourceBundle.getBundle("TestBundle");
+		displayValues(bundle1);
+
+		Locale defaultLocale = Locale.getDefault();
+		ResourceBundle bundle2 = ResourceBundle.getBundle("TestBundle", defaultLocale);
+		displayValues(bundle2);
+
+		Locale swedishLocale = new Locale("sv", "SE");
+		ResourceBundle bundle3 = ResourceBundle.getBundle("TestBundle", swedishLocale);
+		displayValues(bundle3);
+
+		Locale nonexistentLocale = new Locale("xx", "XX");
+		ResourceBundle bundle4 = ResourceBundle.getBundle("TestBundle", nonexistentLocale);
+		displayValues(bundle4);
+
+	}
+
+	public static void displayValues(ResourceBundle bundle) {
+		System.out.println("hello message:" + bundle.getString("my.hello"));
+		System.out.println("goodbye message:" + bundle.getString("my.goodbye"));
+		System.out.println("question message:" + bundle.getString("my.question"));
+		System.out.println();
+	}
+
 }
